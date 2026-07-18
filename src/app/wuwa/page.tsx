a1e5c6730ff1c400c818450f7508f675ac40ef6c
Ivan Kwok <ivnkwok@gmail.com>
2026-07-17 19:26:34 -0700

    bobby

--- src/app/wuwa/page.tsx
+++ src/app/wuwa/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
     <BannerImage
-      src="/wuwa/banner.jpg"
-      alt="Wuwa"
+      src="/wuwa/roverspeed.jpg"
+      alt="Wuthering Waves Banner"
       ></BannerImage>
         <div className="text-center mt-4">
-          <h1 className="text-3xl font-bold">Wuwa</h1>
+          <h1 className="text-3xl font-bold">Wuthering Waves</h1>
           <p className="mt-2 text-lg">A game about a little creature named Wuwa.</p>
         </div>
       </div>
